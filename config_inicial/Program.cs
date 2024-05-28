af645ce9c74ee515e167904e51e1ed70aa3695fe
Vitor Bando <vitorm.804@gmail.com>
2024-05-28 09:13:28 -0300

    Aprendendo sobre arrays e listas, e implementando suas funções.

--- config_inicial/Program.cs
+++ config_inicial/Program.cs
@@ -325,3 +325,58 @@ while (exibirMenu)
 }
 
 Console.WriteLine("O programa se encerrou");
+
+
+int[] arrayInteiros = new int[4];
+arrayInteiros[0] = 72;
+arrayInteiros[1] = 64;
+arrayInteiros[2] = 50;
+arrayInteiros[3] = 1;
+
+//Array.Resize(ref arrayInteiros, arrayInteiros.Length * 2); // Aumentando o tamanho do array
+
+int[] arrayInteirosDobrado = new int[arrayInteiros.Length * 2];
+Array.Copy(arrayInteiros, arrayInteirosDobrado, arrayInteiros.Length);
+
+Console.WriteLine("Percorrendo Array com o For");
+for(int contador = 0; contador < arrayInteiros.Length; contador++)
+{
+    Console.WriteLine($"Posição N° {contador} - {arrayInteiros[contador]}");
+}
+
+Console.WriteLine("Percorrendo Array com o ForEach");
+int contadorForeach = 0;
+foreach (int valor in arrayInteiros)
+{
+    Console.WriteLine($"Posição N° {contadorForeach} - {valor}");
+    contadorForeach++;
+}
+
+List<string> listaString = new List<string>();
+
+listaString.Add("SP");
+listaString.Add("BA");
+listaString.Add("MG");
+listaString.Add("RJ");
+
+
+// Testando implementações das listas
+// Console.WriteLine($"Itens na minha lista: {listaString.Count} - Capacidade: {listaString.Capacity}");
+
+// listaString.Add("SC");
+// Console.WriteLine($"Itens na minha lista: {listaString.Count} - Capacidade: {listaString.Capacity}");
+
+// listaString.Remove("MG");
+// Console.WriteLine($"Itens na minha lista: {listaString.Count} - Capacidade: {listaString.Capacity}");
+
+Console.WriteLine("Percorrendo Lista com o For");
+for(int contador = 0; contador < listaString.Count; contador++)
+{
+    Console.WriteLine($"Posição N° {contador} - {listaString[contador]}");
+}
+Console.WriteLine("Percorrendo Lista com o ForEach");
+foreach (string item in listaString)
+{
+    Console.WriteLine($"Posição N° {contadorForeach} - {item}");
+    contadorForeach++;
+}
--- config_inicial/obj/Debug/net8.0/config_inicial.AssemblyInfo.cs
+++ config_inicial/obj/Debug/net8.0/config_inicial.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("config_inicial")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+55400fabe31ee6e184cdde7e70a45d48be17491b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+13a2b5049f9eab981985783b6237896ffdabcc27")]
 [assembly: System.Reflection.AssemblyProductAttribute("config_inicial")]
 [assembly: System.Reflection.AssemblyTitleAttribute("config_inicial")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
