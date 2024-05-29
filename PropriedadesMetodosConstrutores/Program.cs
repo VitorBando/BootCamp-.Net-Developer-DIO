a2f55fa0446e979d4955102bfdd893369ab79a4d
Vitor Bando <vitorm.804@gmail.com>
2024-05-29 10:35:58 -0300

    Introduzindo Propriedadades, Metodos e Construtores, criamos classes para implementar o que foi ensinado.

---
+++ PropriedadesMetodosConstrutores/Program.cs
@@ -0,0 +1,20 @@
+﻿using PropriedadesMetodosConstrutores.Models;
+
+Pessoa p1 = new Pessoa(nome:"Vitor", sobrenome:"Bando");
+Pessoa p2 = new Pessoa(nome:"Ana", sobrenome:"Rocha");
+
+Curso cursoDeIngles = new Curso();
+cursoDeIngles.Nome = "Ingles";
+cursoDeIngles.Alunos = new List<Pessoa>();
+
+cursoDeIngles.AdicionarAluno(p1);
+cursoDeIngles.AdicionarAluno(p2);
+cursoDeIngles.ListarAlunos();
+
+
+// Pessoa p1 = new Pessoa();
+
+// p1.Nome = "Vitor";
+// p1.Sobrenome = "Bando";
+// p1.Idade = 25;
+// p1.Apresentar();
--- PropriedadesMetodosConstrutores/obj/Debug/net8.0/PropriedadesMetodosConstrutores.AssemblyInfo.cs
+++ PropriedadesMetodosConstrutores/obj/Debug/net8.0/PropriedadesMetodosConstrutores.AssemblyInfo.cs
@@ -10,12 +10,12 @@
 using System;
 using System.Reflection;
 
-[assembly: System.Reflection.AssemblyCompanyAttribute("config_inicial")]
+[assembly: System.Reflection.AssemblyCompanyAttribute("PropriedadesMetodosConstrutores")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1fa8f559ddd6251789c1dd491ee1088594e559d2")]
-[assembly: System.Reflection.AssemblyProductAttribute("config_inicial")]
-[assembly: System.Reflection.AssemblyTitleAttribute("config_inicial")]
+[assembly: System.Reflection.AssemblyProductAttribute("PropriedadesMetodosConstrutores")]
+[assembly: System.Reflection.AssemblyTitleAttribute("PropriedadesMetodosConstrutores")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
 
 // Gerado pela classe WriteCodeFragment do MSBuild.
